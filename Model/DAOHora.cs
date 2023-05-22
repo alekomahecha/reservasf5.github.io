9ae07a078d6f8e4a0ef32d65b5de2a6f801f819a
alekomahecha <alecomahecha92@gmail.com>
2023-05-21 22:41:44 -0500

    Agrego DAO y negocio de Horas

--- Model/DAOHora.cs
+++ Model/DAOHora.cs
@@ -1,12 +1,143 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
-    internal class DAOHora
+    public class DAOHora
     {
+        public int id;
+        public string hora;
+        public string precio;
+        public int idCancha;
+
+        conexion cn = new conexion();
+
+        public int RegistrarHora()
+        {
+            int Resultado = 0;
+            try
+            {
+                id = GenerarID();
+                string query = "INSERT INTO `hora`(`id`, `hora`, `precio`, `estado`, `fecharegistro`, `idcancha`) VALUES (" +
+                    id + ",'"
+                    + hora + "','"
+                    + precio + "','"                    
+                    + 1 + "','"
+                    + DateTime.UtcNow.ToString() + "',"
+                    + idCancha + ")";
+
+                if (cn.conectar() == true)
+                {
+                    Resultado = cn.TransaccionBase(query);
+                    Resultado = id;
+                }
+            }
+            catch (Exception ex)
+            {
+                Resultado = 0;
+                throw;
+            }
+            return Resultado;
+        }
+        public Boolean ModificarHora()
+        {
+            Boolean Resultado = false;
+            Int32 valor;
+            try
+            {
+                string query = "UPDATE `hora` SET " +
+                    "hora= '" + hora + "'," +
+                    "precio= '" + precio + "'," +                    
+                    "Estado= '" + 1 + "'," +
+                    "fechaRegistro= '" + DateTime.UtcNow.ToString() + "' " +
+                    " WHERE id = " + id;
+
+                if (cn.conectar() == true)
+                {
+                    valor = cn.TransaccionBase(query);
+                    Resultado = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Resultado = false;
+                throw;
+            }
+            return Resultado;
+        }
+        public Boolean EliminarHora()
+        {
+            Boolean Resultado = false;
+            Int32 valor;
+            try
+            {
+                string query = "UPDATE `hora` SET " +
+                    "Estado= '" + 0 + "'," +
+                    "fechaRegistro= '" + DateTime.UtcNow.ToString() + "' " +
+                    " WHERE id = " + id;
+
+                if (cn.conectar() == true)
+                {
+                    valor = cn.TransaccionBase(query);
+                    Resultado = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Resultado = false;
+                throw;
+            }
+            return Resultado;
+        }
+        public DataTable ConsultaHora()
+        {
+            DataTable tablaRetorno = new DataTable();
+            try
+            {
+                string query = "select * from hora";
+                //string query = "select max(codigo) as id from departamento";
+                if (cn.conectar() == true)
+                {
+                    tablaRetorno = cn.Consultar(query);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return tablaRetorno;
+        }
+        public int GenerarID()
+        {
+            int Count = 0;
+            try
+            {
+                DataTable tablaRetorno = new DataTable();
+                string query = "select max(id) as id from hora";
+                //string query = "select max(codigo) as id from departamento";
+                if (cn.conectar() == true)
+                {
+                    tablaRetorno = cn.Consultar(query);
+                    foreach (DataRow item in tablaRetorno.Rows)
+                    {
+                        if (item[0].ToString() != String.Empty)
+                            Count = Convert.ToInt32(item[0].ToString()) + 1;
+                        else
+                            Count = 1;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return Count;
+
+        }
+
     }
 }
--- Negocio/Hora.cs
+++ Negocio/Hora.cs
@@ -1,12 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Negocio
 {
-    internal class Hora
+    public class Hora
     {
+        public int Registrar(String pHora,String pPrecio,int pIdcancha)
+        {
+
+            int result = 0;
+            Model.DAOHora cx = new Model.DAOHora();
+            try
+            {
+                cx.hora = pHora;
+                cx.precio = pPrecio;
+                cx.idCancha = pIdcancha;               
+                result = cx.RegistrarHora();
+            }
+            catch (Exception ex)
+            {
+                result = 0;
+                throw ex;
+            }
+            return result;
+        }
+
+        public Boolean Modificar(Int32 pId, String pHora, String pPrecio)
+        {
+
+            Boolean result = false;
+            Model.DAOHora cx = new Model.DAOHora();
+            try
+            {
+                cx.id = pId;
+                cx.hora = pHora;
+                cx.precio = pPrecio;
+                cx.ModificarHora();
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                throw ex;
+            }
+            return result;
+        }
+
+        public Boolean Eliminar(Int32 pId)
+        {
+            Boolean result = false;
+            Model.DAOHora cx = new Model.DAOHora();
+            try
+            {
+                cx.id = pId;
+                cx.EliminarHora();
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                throw ex;
+            }
+            return result;
+        }
+        public DataTable Consultar()
+        {
+            DataTable dt = new DataTable();
+            Model.DAOHora cx = new Model.DAOHora();
+            try
+            {
+                dt = cx.ConsultaHora();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return dt;
+        }
+
     }
 }
